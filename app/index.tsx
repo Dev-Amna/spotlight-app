04d85a9252a4252e214fe0684badcf3d2a27849b
Amna Ashraf <amnaharaf272@gmail.com>
2026-04-20 14:10:46 +0500

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from "./styles/auth.styles";
+
+const Index = () => {
+  return (
+    <View style={styles.containe}>
+      <Text style={styles.textStyle}>Hello Amna Welcome Back!</Text>
+    </View>
+  )
+}
+
+export default Index
+
---
+++ app/styles/auth.styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+export const styles = StyleSheet.create({
+    containe: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    textStyle: {
+        color: "red",
+        fontSize: 20 
+    }
+})
